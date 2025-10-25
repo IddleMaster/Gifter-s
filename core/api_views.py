37878a43e347e71a709c7e11b33c0983f2f11bb9
Elias <el.alcaide@duocuc.cl>
2025-10-24 22:45:45 -0300

    CAMBIOSS

---
+++ core/api_views.py
@@ -0,0 +1,15 @@
+# from django.urls import path
+# from .api_views import (
+#     NotificacionListCreateView,
+#     NotificacionDetailView,
+#     MarkAllReadView,
+#     SummaryView,
+# )
+# 
+# urlpatterns = [
+#     path("notificaciones/", NotificacionListCreateView.as_view()),
+#     path("notificaciones/<int:notificacion_id>/", NotificacionDetailView.as_view()),
+#     path("notificaciones/marcar-todo-leido/", MarkAllReadView.as_view()),
+#     path("notificaciones/unread-summary/", SummaryView.as_view()),
+# ]
+# 
